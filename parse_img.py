dde1b5bcdb855fb8216af448e4cb87aa6e037ba3
master <hulktv222@mail.ru>
2021-04-18 17:44:26 +0300

    parser

---
+++ parse_img.py
@@ -0,0 +1,18 @@
+file_path = "templates/"
+file_name_postfix = ".html"
+files = ["catalog", "home", "index", "product"]
+need_find = "\"img/"
+need_find2 = "\"img/"
+replace = "\"static/img/"
+for file_name in files:
+    new_file = ""
+    with open(file_path + file_name + file_name_postfix, "r", encoding='utf-8') as file:
+        lines = file.readlines()
+        for line in lines:
+            if need_find in line:
+                line = line.replace(need_find, replace)
+            new_file += line
+    with open(file_path + file_name + file_name_postfix, "w" , encoding='utf-8') as file:
+        file.writelines(new_file)
+
+
