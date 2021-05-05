f7b81d920edc36b21d2fd8203f9f6ca9f8f202ac
a-small-coder <61427295+a-small-coder@users.noreply.github.com>
2021-05-05 15:49:56 +0300

    Revert "Logomarketbackend"

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for
-from jinja2 import Template
 
 app = Flask(__name__)
 
@@ -9,21 +8,5 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/product')
-def product():
-    return render_template("product.html")
-
-
-@app.route('/catalog')
-def catalog():
-    return render_template("catalog.html")
-
-
-@app.route('/checkout')
-def checkout():
-    return render_template("checkout.html")
-
-
 if __name__ == "__main__":
-
     app.run(debug=True)
