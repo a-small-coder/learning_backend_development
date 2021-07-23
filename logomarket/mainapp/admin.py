3370e7455748c725595544bb7ffb50ac7e527411
FairaIchsa <zelov02@yandex.ru>
2021-07-23 18:53:33 +0300

    made models (without subcategories)

--- logomarket/mainapp/admin.py
+++ logomarket/mainapp/admin.py
@@ -1,3 +1,36 @@
+from django.forms import ModelChoiceField
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class TreadmillAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='treadmills'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class BallAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='balls'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class TennisTableAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='tennis_tables'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Treadmill, TreadmillAdmin)
+admin.site.register(Ball, BallAdmin)
+admin.site.register(TennisTable, TennisTableAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
--- logomarket/mainapp/models.py
+++ logomarket/mainapp/models.py
@@ -1,3 +1,108 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+User = get_user_model()
 
 # Create your models here.
+
+#*********************
+#1 Category
+#2 Product
+#3 CartProduct
+#4 Cart
+#5 Order
+#*********************
+#6 Customer
+#7 Specification
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    class Meta:
+        abstract = True
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    description = models.TextField(verbose_name='Описание', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.title
+
+
+class Treadmill(Product):
+
+    weight = models.CharField(max_length=255, verbose_name='Вес дорожки')
+    max_weight = models.CharField(max_length=255, verbose_name='Максимальный вес пользователя')
+    max_speed = models.CharField(max_length=255, verbose_name='Максимальная скорость')
+    engine_power = models.CharField(max_length=255, verbose_name='Мощность двигателя')
+
+    def __str__(self):
+        return "{}: {}".format(self.category.name, self.title)
+
+
+class Ball(Product):
+    weight = models.CharField(max_length=255, verbose_name='Вес мяча')
+    diameter = models.CharField(max_length=255, verbose_name='Диаметр')
+    material = models.CharField(max_length=255, verbose_name='Материал камеры')
+
+    def __str__(self):
+        return "{}: {}".format(self.category.name, self.title)
+
+
+class TennisTable(Product):
+
+    weight = models.CharField(max_length=255, verbose_name='Вес стола')
+    material = models.CharField(max_length=255, verbose_name='Материал столешницы')
+    size = models.CharField(max_length=255, verbose_name='Размеры')
+
+    def __str__(self):
+        return "{}: {}".format(self.category.name, self.title)
+
+
+class CartProduct(models.Model):
+
+    user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    qty = models.PositiveIntegerField(default=1, verbose_name='Количество')
+    total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Сумма')
+
+    def __str__(self):
+        return "Продукт: {} (для корзины)".format(self.product.title)
+
+
+class Cart(models.Model):
+
+    owner = models.ForeignKey('Customer', verbose_name='Владелец корзины', on_delete=models.CASCADE)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
+    total_products = models.PositiveIntegerField(default=0)
+    total_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Сумма')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Customer(models.Model):
+
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
+
+    def __str__(self):
+        return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
